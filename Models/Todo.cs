542fc5b88fde61c7cad443c8ccd1205880690813
Yakup Geyik <yakuppgeyik@gmail.com>
2025-12-19 00:36:26 +0300

    refactor: extract common model properties to BaseEntity

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,13 +1,10 @@
 namespace FirstWebApi.Models;
 
-public class Todo
+public class Todo : BaseEntity
 {
-  public int Id { get; set; }
   public string Title { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
-  public bool IsCompleted { get; set; }
-  public DateTime CreatedAt { get; set; }
-  public DateTime? UpdatedAt { get; set; }
+  public bool IsCompleted { get; set; } = false;
 
   public int UserId { get; set; }
   public User? User { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,8 @@
 namespace FirstWebApi.Models;
 
-public class User
+public class User : BaseEntity
 {
-  public int Id { get; set; }
   public string Username { get; set; } = string.Empty;
   public string PasswordHash { get; set; } = string.Empty;
   public string Email { get; set; } = string.Empty;
-  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
